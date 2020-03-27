d62962d8558ce296a857b4d9015c98537ed747a0
unknown <you@example.com>
2020-03-27 20:08:01 +0400

    first-counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  plus = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  minus = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1> {this.state.count}</h1>
+        <button onClick={this.plus} className="plus ">
+          +
+        </button>
+        <h2>Please push button</h2>
+
+        <button onClick={this.minus} className="minus ">
+          -
+        </button>
+      </div>
+    );
+  }
 }
-
-export default App;
